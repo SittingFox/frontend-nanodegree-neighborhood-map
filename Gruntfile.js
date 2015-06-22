84b79f4d4640d756ad074409be2eee18e01ac623
Sitting Fox <SittingFox@users.noreply.github.com>
2015-06-22 15:55:57 -0400

    chore: Add grunt-shell for running Bower update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-bower');
 
   grunt.initConfig({
+    // Move Bower compnents to better location
     bower: {
       dev: {
         dest: 'js/lib/',
@@ -9,10 +11,18 @@ module.exports = function(grunt) {
           keepExpandedHierarchy: false
         }
       }
+    },
+
+    // Update Bower packages
+    shell: {
+        bower: {
+            command: 'bower update'
+        }
     }
   });
 
   grunt.registerTask('default', [
+    'shell',
     'bower'
     ]);
 };
